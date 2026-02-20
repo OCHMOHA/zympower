58c357a494fb7749cd4f62a9c09d6e34cd9afefb
OCHMOHA <1132245115@pfur.ru>
2026-02-20 19:42:17 +0000

    meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,37 +27,49 @@ export default function RootLayout({
     <html lang="fr">
       <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {/* Meta Pixel Code */}
-        {pixelId && (
-          <>
-            <Script
-              id="facebook-pixel"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${pixelId}');
-                  fbq('track', 'PageView');
-                `,
-              }}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              // Existing pixel from environment, if provided
+              ${pixelId ? `fbq('init', '${pixelId}');` : ""}
+
+              // Additional hard-coded pixel
+              fbq('init', '4042948302780784');
+
+              // Track PageView on all initialized pixels
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {pixelId && (
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+              alt="facebook-pixel-env"
             />
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: "none" }}
-                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-                alt="facebook-pixel"
-              />
-            </noscript>
-          </>
-        )}
+          )}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=4042948302780784&ev=PageView&noscript=1"
+            alt="facebook-pixel-4042948302780784"
+          />
+        </noscript>
         {/* End Meta Pixel Code */}
         {children}
         <Analytics />
